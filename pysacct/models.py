7ddbd8f729c83f5f93f7f52f8d9ff9fd8e8f7d63
bwhicks <benjamin.w.hicks@gmail.com>
2017-06-22 01:20:45 -0400

    Much simplified Job class and tests

--- pysacct/models.py
+++ pysacct/models.py
@@ -1,6 +1,8 @@
 """
 Models to represent SLURM jobs from sacct in a groovy Pythonic way.
 """
+import json
+
 VALID_SACCT_FIELDS = [
     'Account', 'AdminComment', 'AllocCPUS', 'AllocGRES', 'AllocNodes',
     'AllocTRES', 'AssocID', 'AveCPU', 'AveCPUFreq', 'AveDiskRead',
@@ -21,38 +23,79 @@ VALID_SACCT_FIELDS = [
 ]
 
 
+def validate_fields(obj, **kwargs):
+    for key, value in kwargs.items():
+        if key.lower() not in (field.lower() for field in
+                               VALID_SACCT_FIELDS):
+            raise ValueError(
+                (
+                    '%s is not a valid field. Please choose from:\n'
+                    '%s' % (key, ', '.join(VALID_SACCT_FIELDS))
+                )
+            )
+        setattr(obj, key.lower(), value)
+
+
+'''
 class Step(object):
     """An object presentation of a single sacct line which requires
-    kwargs or a dict following the same format as `sacct -P`"""
+    kwargs following the same format as `sacct -P`"""
 
-    def __init__(self, sacct_line=None, **kwargs):
-
-        for key, value in kwargs.items():
-            if key.lower() not in (field.lower() for field in
-                                   VALID_SACCT_FIELDS):
-                raise ValueError(
-                    (
-                        '%s is not a valid field. Please choose from:\n'
-                        '%s' % (key, ', '.join(VALID_SACCT_FIELDS))
-                    )
-                )
-            setattr(self, key.lower(), value)
+    def __init__(self, **kwargs):
+        validate_fields(self, **kwargs)
 
     def __repr__(self):
         return '<Step: %s>' % self.jobid
 
+    def __str__(self):
+        return '<Step: %s>' % self.jobid
+
+    def to_JSON(self):
+        return json.dumps(self.__dict__)
+
+'''
+
 
 class Job(object):
+    """
+    Object representation of a SLURM job, with any steps as `Job.jobsteps`.
+    Can be created from kwargs or dict passed as kwargs.
+    """
 
-    def __init__(self, step):
-        if not isinstance(step, Step):
-            raise TypeError('Job must be initialized from Step object')
+    def __init__(self, **kwargs):
+        validate_fields(self, **kwargs)
 
-        for k, v in step.__dict__.items():
-            setattr(self, k, v)
+    def __iter__(self):
+        for key, value in self.__dict__.items():
+            yield key.strip('_'), value
 
     def __repr__(self):
-        return '<Job: %s>' % self.jobid
+        return '<Job: %s>' % dict(self)
+
+    def __str__(self):
+        return "<Job: {'jobid': %s}>" % self.jobid
+
+    # Fall back for a valid field that is empty
+    def __getattr__(self, name):
+        if name.lower() in (field.lower() for field in
+                   VALID_SACCT_FIELDS):
+            return None
+        raise ValueError('%s is not a valid field for a Job object' % name)
+
+    @property
+    def jobid(self):
+        return self._jobid
+
+    @jobid.setter
+    def jobid(self, value):
+        try:
+            self._jobid = int(value)
+        except ValueError:
+            self._jobid = value
+
+    @jobid.deleter
+    def jobid(self):
+        del self._jobid
 
     @property
     def jobsteps(self):
@@ -61,15 +104,25 @@ class Job(object):
     @jobsteps.setter
     def jobsteps(self, value):
         if not isinstance(value, list):
-            raise TypeError("jobsteps must be of type list")
+            raise TypeError("Jobsteps must be of type list")
         for item in value:
-            if not isinstance(item, Step):
-                raise ValueError('%s is not a Step object' % item)
-            if (item.jobid).split('.')[0] != self.jobid:
-                raise ValueError('jobstep must match the jobid of the associated job')
+            if not isinstance(item, Job):
+                raise TypeError('%s is not a Job object' % item)
+            if (str(item.jobid)).split('.')[0] != str(self.jobid):
+                raise ValueError('Job and jobstep must match on JobID')
 
         self._jobsteps = value
 
     @jobsteps.deleter
     def jobsteps(self):
         del self._jobsteps
+
+    def to_JSON(self):
+        jobsteps = []
+        if self.jobsteps:
+            jobsteps = [dict(step) for step in self.jobsteps]
+            delattr(self, 'jobsteps')
+
+        job_dict = dict(self)
+        job_dict['jobsteps'] = jobsteps
+        return json.dumps(job_dict)
--- pysacct/tests/models_test.py
+++ pysacct/tests/models_test.py
@@ -1,30 +1,65 @@
 from unittest import TestCase
-from pysacct.models import Job, Step, VALID_SACCT_FIELDS
-
-
-class TestStep(TestCase):
-
-    def testrepr(self):
-        step = Step(jobid='123')
-        assert repr(step) == '<Step: 123>'
-
-    def test_validation(self):
-        with self.assertRaises(ValueError) as context:
-            Step(foo='123')
-        assert('foo is not a valid field' in str(context.exception))
-        assert(', '.join(VALID_SACCT_FIELDS) in str(context.exception))
+from pysacct.models import Job, VALID_SACCT_FIELDS
 
 
 class TestJob(TestCase):
 
-    def testrepr(self):
-        step = Step(jobid='123')
-        job = Job(step)
-        assert repr(job) == '<Job: 123>'
+    def test_init(self):
+        # Making a job with default settings should work
+        job = Job(jobid='123')
+        assert isinstance(job, Job)
 
-    def test_validation(self):
-        # Should raise an error because it is not value Step
+        # Making one with a field not supported by sacct shouldn't
+        with self.assertRaises(ValueError) as context:
+            job = Job(jobid='123', foo='bar')
+        assert 'foo is not a valid field.' in str(context.exception)
+        assert ', '.join(VALID_SACCT_FIELDS) in str(context.exception)
+
+    def test_getattr(self):
+        job = Job(jobid='123')
+        # Nor should adding one
+        with self.assertRaises(ValueError) as context:
+            job.foo
+        assert 'foo is not a valid field' in str(context.exception)
+
+    def test_repr(self):
+        job = Job(jobid='123', user='bhicks')
+        assert repr(job) == "<Job: {'user': 'bhicks', 'jobid': 123}>"
+
+    def test_str(self):
+        job = Job(jobid='123', user='bhicks')
+        assert str(job) == "<Job: {'jobid': 123}>"
+
+    def test_jobid(self):
+        job = Job(jobid='123')
+        # Should be an int because it can be converted
+        assert isinstance(job.jobid, int)
+        # Should remain a str because it can't be
+        job = Job(jobid='123.batch')
+        assert isinstance(job.jobid, str)
+
+        # Delete works as expected
+        del job.jobid
+        assert not hasattr(job, 'jobid')
+
+    def test_jobsteps(self):
+        # A job and its step with the same jobid before . can be matched
+        job = Job(jobid='123')
+        job_step = Job(jobid='123.batch')
+        job.jobsteps = [job_step]
+        assert job.jobsteps == [job_step]
+
+        # Adding anything other than a list should result in a type error
         with self.assertRaises(TypeError) as context:
-            Job('123')
-        assert ('Job must be initialized from Step object'
-                in str(context.exception))
+            job.jobsteps = ''
+        assert 'Jobsteps must be of type list' in str(context.exception)
+
+        # Adding a list with non Job objects should produce a TypeError
+        with self.assertRaises(TypeError) as context:
+            job.jobsteps = ['foo', 'bar']
+        assert 'foo is not a Job object' in str(context.exception)
+
+        # Adding a job that doesn't have the same JobID should fail
+        with self.assertRaises(ValueError) as context:
+            job.jobsteps = [Job(jobid='456')]
+        assert 'Job and jobstep must match on JobID' in str(context.exception)
